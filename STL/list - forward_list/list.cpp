93561960ae1040332122a6161f2227904185fb95
Parth Gupta <neelaru8@gmail.com>
2023-08-27 07:59:58 +0530

    initital commit

---
+++ STL/list - forward_list/list.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+//list -> implements a doubly linked list
+//forward_list -> implements a singly linked list
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	forward_
+}
